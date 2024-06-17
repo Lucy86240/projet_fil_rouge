005b0f2278ff98d7782242d5624b841e366bda7b
Lucy86240 <raye.lucy@live.fr>
2024-06-17 15:57:38 +0200

    add verifications form signup

---
+++ js/auth/signup.js
@@ -0,0 +1,85 @@
+const inputNom = document.getElementById("SurnameInput")
+const inputPrenom = document.getElementById("firstnameInput")
+const inputMail = document.getElementById("EmailInput")
+const inputPassword = document.getElementById("PasswordInput")
+const inputValidatePassword = document.getElementById("ValidatePasswordInput")
+const btnValidate = document.getElementById("btn-validation-inscription")
+
+
+inputNom.addEventListener("keyup", validateForm);
+inputPrenom.addEventListener("keyup", validateForm);
+inputMail.addEventListener("keyup", validateForm);
+inputPassword.addEventListener("keyup", validateForm);
+inputValidatePassword.addEventListener("keyup", validateForm);
+
+function validateForm(){
+    const nomOk = validateRequired(inputNom);
+    const prenomOk = validateRequired(inputPrenom);
+    const mailOk = validateMail(inputMail);
+    const passwordOk = validatePassword(inputPassword);
+    const validePwdOk = validateConfirmationPassword(inputPassword, inputValidatePassword);
+
+    if(nomOk && prenomOk && mailOk && passwordO && validePwdOk){
+        btnValidate.disabled = false;
+    }
+    else{
+        btnValidate.disabled = true;
+    }
+}
+
+function validateConfirmationPassword(inputPwd, inputConfirmPwd){
+    if(inputPwd.value == inputConfirmPwd.value){
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        inputConfirmPwd.classList.add("is-invalid");
+        inputConfirmPwd.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validatePassword(input){
+    //Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validateMail(input){
+    //regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if (mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function validateRequired(input){
+    if(input.value != ''){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
+    }
+}
