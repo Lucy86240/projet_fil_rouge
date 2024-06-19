465ea39999426278b4ce757e3bc587251ab3d9b1
Lucy86240 <raye.lucy@live.fr>
2024-06-19 09:01:20 +0200

    add roles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 const tokenCookieName = "accesstoken";
+const RoleCookieName = "role"
+const signoutBtn = document.getElementById("signout-btn");
+
+signoutBtn.addEventListener("click, signout");
+
+function getRole(){
+    return getCookie(RoleCookieName);
+}
+
+function signout(){
+    eraseCookie(tokenCookieName);
+    eraseCookie(RoleCookieName);
+    window.location.reload();
+}
+
+
 function setToken(token){
     setCookie(tokenCookieName, token, 7);
 }
@@ -36,4 +52,37 @@ function isConnected(){
     else{
         return true;
     }
+}
+
+function showAndHideElementsForRoles(){
+    const userConnected = isConnected();
+    const role = getRole();
+
+    let allElementsToEdit = document.querySelectorAll('[data-show]');
+
+    allElementsToEdit.forEach(element => {
+        switch(element.dataset.show){
+            case 'disconnected' :
+                if(userConnected){
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'connected':
+                if(!userConnected){
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'admin' :
+                if(!userConnected || role != "admin"){
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'client':
+                if(!userConnected || role != "client"){
+                    element.classList.add("d-none");
+                }
+                break;
+
+        }
+    })
 }
